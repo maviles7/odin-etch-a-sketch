b2718b3f549d9f8b4319ab3498bb79431fbe6cd3
miriam <maviles221@gmail.com>
2025-03-07 13:24:28 -0600

    clear

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// create a div in the body of the html to insert a new div into 
+// this is the div that will be manipulated w/JS 
+
+
+
+// if mouse hovers over the div, the div will change color
+
+initGrid();
+
+function initGrid() {
+    let gridSize = 16;
+};
