5011a4a369952fbf50432249b56aaf720953416f
miriam <maviles221@gmail.com>
2025-03-07 13:31:54 -0600

    js initial commit take2

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // create a div in the body of the html to insert a new div into 
 // this is the div that will be manipulated w/JS 
 
-
+const gridContainer = document.querySelector('.grid-container');
 
 // if mouse hovers over the div, the div will change color
 
